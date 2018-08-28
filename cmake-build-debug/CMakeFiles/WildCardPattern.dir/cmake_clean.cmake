c3dc2918bdaffc43079b21db524c35346e3887db
YuriySavchenko <yurec.proger@gmail.com>
2018-08-28 14:45:51 +0300

    Upload hidden files

---
+++ cmake-build-debug/CMakeFiles/WildCardPattern.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WildCardPattern.dir/main.cpp.o"
+  "CMakeFiles/WildCardPattern.dir/wildcard.cpp.o"
+  "WildCardPattern.pdb"
+  "WildCardPattern"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WildCardPattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
